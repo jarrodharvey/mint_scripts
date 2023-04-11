17d5e95a3b550ccff1bc423a30736b16a9d3bc00
jarrodharvey <jarrodharvey@gmail.com>
2023-04-11 22:02:44 +1000

    Added spell checking to tts

--- transcribe_text.sh
+++ transcribe_text.sh
@@ -5,6 +5,7 @@ start_dir=$(pwd)
 nd_dir="/home/jarrod/git_installs/nerd-dictation"
 
 tmp_file="/tmp/transcription.txt"
+tmp_spellchecked="/tmp/transcription_spellchecked.txt"
 
 cd $nd_dir
 
@@ -18,14 +19,19 @@ gedit $tmp_file
 
 ./nerd-dictation end
 
+notify-send "Making punctuation fixes..."
+
+java -jar /snap/languagetool/38/usr/bin/languagetool-commandline.jar --apply $tmp_file > $tmp_spellchecked
+
 notify-send "Make any required edits"
 
-vim $tmp_file
+vim $tmp_spellchecked
 
-cat "$tmp_file" | xclip -selection clipboard
+cat "$tmp_spellchecked" | xclip -selection clipboard
 
 notify-send "Text copied to clipboard"
 
 rm $tmp_file
+rm $tmp_spellchecked
 
 cd $start_dir
